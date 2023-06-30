40a370edf9c07f6fbe686ea27681b59e1f785026
Lee Sang Hyeon <francesco419@naver.com>
2023-06-30 22:17:30 +0900

    230630

---
+++ src/functions/dragMap.ts
@@ -0,0 +1,63 @@
+import { LocationType } from './interface/interface';
+
+let _startX = 0;
+let _startY = 0;
+let _offsetX = 0;
+let _offsetY = 0;
+let _dragElement: HTMLDivElement | undefined;
+let loaction: LocationType;
+
+const debounceOnChange = _.debounce(() => {
+  //dispatch(setPageLocation(loaction));
+}, 500);
+
+function onMouseDown(event: React.MouseEvent<HTMLDivElement>) {
+  _startX = event.clientX;
+  _startY = event.clientY;
+  _dragElement = document.getElementById('dragSection') as HTMLDivElement;
+  _offsetX = _dragElement.offsetLeft;
+  _offsetY = _dragElement.offsetTop;
+  console.log('page start');
+}
+
+function onMouseMoveHandler(event: React.MouseEvent<HTMLDivElement>) {
+  if (_dragElement) {
+    loaction = {
+      x: _offsetX + event.clientX - _startX,
+      y: _offsetY + event.clientY - _startY
+    };
+    _dragElement.style.left = loaction.x + 'px';
+    _dragElement.style.top = loaction.y + 'px';
+
+    console.log('page drag');
+    debounceOnChange();
+  }
+}
+
+function onMouseUp(event: React.MouseEvent<HTMLDivElement>) {
+  if (_dragElement) {
+    const y = _offsetY + event.clientY - _startY * pageStyle.value.scale;
+    const x = _offsetX + event.clientX - _startX * pageStyle.value.scale;
+    document.onmousemove = null;
+    if (y > 0) {
+      _dragElement.style.top = 0 + 'px';
+    }
+    if (x > 0) {
+      _dragElement.style.left = 0 + 'px';
+    }
+    //상단 범위 내부로 전체 엘리먼트가 들어올시 (0,0)으로 강제 이동.
+    if (window.innerHeight > pageStyle.value.height + y) {
+      _dragElement.style.top =
+        (window.innerHeight - pageStyle.value.height) * pageStyle.value.scale +
+        'px';
+    }
+
+    if (window.innerWidth > pageStyle.value.width + x) {
+      _dragElement.style.left =
+        (window.innerWidth - pageStyle.value.width) * pageStyle.value.scale +
+        'px';
+    }
+  }
+  _dragElement = undefined;
+  console.log('page end');
+}
