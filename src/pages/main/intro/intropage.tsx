fd88c3ba45edbca6c5dde6843038a3e8cf777f6d
Lee Sang Hyeon <francesco419@naver.com>
2023-07-09 00:00:56 +0900

    230709

--- src/pages/main/intro/intropage.tsx
+++ src/pages/main/intro/intropage.tsx
@@ -16,6 +16,26 @@ export default function IntroPage() {
             <button>MindMap</button>
           </div>
         </div>
+        <div className='intropage__intro'>
+          <div>
+            <p>----text----</p>
+          </div>
+          <div>
+            <p>img</p>
+          </div>
+        </div>
+        <div className='intropage__use'>
+          <p>----How to use----</p>
+          <p>IMg</p>
+        </div>
+        <div className='intropage__update'>
+          <p>----Updates----</p>
+          <p>update detail</p>
+        </div>
+        <div className='intropage__contact'>
+          <p>----contact----</p>
+          <p>contact detail</p>
+        </div>
       </div>
     </div>
   );
