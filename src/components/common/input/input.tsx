48b6b710ac4d9db680b67fcb98b3255691a9a498
Lee Sang Hyeon <francesco419@naver.com>
2023-07-24 23:41:43 +0900

    230724

--- src/components/common/input/input.tsx
+++ src/components/common/input/input.tsx
@@ -1,3 +1,67 @@
-export default function Input() {
-  return <div></div>;
+import styled, { css, FlattenSimpleInterpolation } from 'styled-components';
+import { ButtonProps, sizeType } from '../../../functions/interface/interface';
+
+const SIZES: sizeType = {
+  sm: css`
+    height: 40px;
+    font-size: 1.2rem;
+    width: 200px;
+  `,
+  md: css`
+    height: 50px;
+    font-size: 1.2rem;
+    width: 180px;
+  `,
+  lg: css`
+    height: 70px;
+    font-size: 1.2rem;
+    width: 200px;
+  `
+};
+
+type InputProps = {
+  style: any;
+  disabled: boolean;
+  type: string;
+  name: string;
+  value: string | number;
+  size: string;
+  onChange: () => void;
+};
+
+export default function Input({
+  style,
+  disabled = false,
+  type,
+  name,
+  value,
+  size,
+  onChange
+}: InputProps) {
+  const sizeStyle = SIZES[size];
+  return (
+    <StyleInput
+      style={style}
+      disabled={disabled}
+      type={type}
+      sizeStyle={sizeStyle}
+      onChange={onChange}
+      name={name}
+      value={value}
+    />
+  );
 }
+
+const StyleInput = styled.input<{ sizeStyle: any }>`
+  ${(p) => p.sizeStyle}
+  padding: 10px;
+  background-color: #2fd883;
+  border: none;
+  border-radius: 10px;
+  color: #fff;
+  font-weight: 700;
+
+  &:hover {
+    filter: brightness(90%);
+  }
+`;
